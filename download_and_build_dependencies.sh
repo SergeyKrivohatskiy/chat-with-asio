e3d48be875b6365af7026287ed872e5543449f08
Sergey Krivohatskiy <s.krivohatskiy@gmail.com>
2015-12-09 12:23:15 +0400

    build fixes added

--- download_and_build_dependencies.sh
+++ download_and_build_dependencies.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 #protobuf
+rm -rf protobuf
 git clone https://github.com/google/protobuf.git
 cd protobuf
 git checkout v2.6.1 -b v2.6.1
@@ -12,9 +13,11 @@ make install
 cd ..
 
 #boost
+rm -rf boost
 git clone --recursive https://github.com/boostorg/boost.git
 cd boost
 git checkout boost-1.59.0 -b boost-1.59.9
-./bootstrap.sh
-./bjam stage install --prefix=$PWD/install_out
+./bootstrap.sh --includedir=$PWD/install_out/include --libdir=/$PWD/install_out/lib
+./bjam install
+cp -R -f libs/asio/include -t install_out/include
 cd ..
